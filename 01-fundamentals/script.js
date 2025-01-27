29c6890b6386b80cd3b57fa983e1a25ebee3836e
Joseph Careless <gjwer@spacer>
2025-01-27 21:47:11 +0100

    switch statement

--- 01-fundamentals/script.js
+++ 01-fundamentals/script.js
@@ -8,3 +8,29 @@ const language = "Polish";
 let population = 2;
 
 console.log(typeof population, typeof language)
+
+day = Number(prompt("what day it is?"))
+function fun(x) {
+  return x
+}
+if (day === 1) {
+    console.log("it's monday");
+} else if (day === 2) {
+    console.log === "it's tuesday";
+} else if (day === 3) {
+    console.log === "it's wednesday";
+} else {
+    console.log("idk what day it is")
+}
+
+switch (day) {
+    case 1:
+        console.log("it's monday");
+        break
+    case 2:
+        console.log("it's tuesday");
+        break
+    case 3: 
+        console.log("it's wednesday");
+  
+}
