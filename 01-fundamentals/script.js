9f013f8a701d9fc72d122f704788d36145f6c185
Joseph Careless <gjwer@spacer>
2025-01-27 21:50:49 +0100

    Template strings

--- 01-fundamentals/script.js
+++ 01-fundamentals/script.js
@@ -1,36 +1,50 @@
-let js = "amazing";
+let js = `amazing`;
 
-let firstName = "Jonas";
+let firstName = `Jonas`;
 console.log(firstName);
-let PI = "3.14";
-const country = "Poland";
-const language = "Polish";
+let PI = `3.14`;
+const country = `Poland`;
+const language = `Polish`;
 let population = 2;
 
 console.log(typeof population, typeof language)
 
-day = Number(prompt("what day it is?"))
-function fun(x) {
-  return x
-}
-if (day === 1) {
-    console.log("it's monday");
-} else if (day === 2) {
-    console.log === "it's tuesday";
-} else if (day === 3) {
-    console.log === "it's wednesday";
-} else {
-    console.log("idk what day it is")
-}
+// day = Number(prompt(`what day it is?`))
+// function fun(x) {
+//   return x
+// }
+// if (day === 1) {
+//     console.log(`it's monday`);
+// } else if (day === 2) {
+//     console.log === `it's tuesday`;
+// } else if (day === 3) {
+//     console.log === `it's wednesday`;
+// } else {
+//     console.log(`idk what day it is`)
+// }
+
+day = promp("what day it is?")
 
 switch (day) {
     case 1:
-        console.log("it's monday");
-        break
+        console.log(`it' ${day}`);
+        break;
     case 2:
-        console.log("it's tuesday");
-        break
+        console.log(`it's ${day}`);
+        break;
     case 3: 
-        console.log("it's wednesday");
-  
+        console.log(`it's ${day}`);
+        break;
+    case 4:
+        console.log(`it's ${day}`);
+        break;
+    case 5:
+        console.log(`it's ${day}`);
+        break;
+    case 6:
+        console.log(`it's ${day}`);
+        break;
+    case 7:
+        console.log(`it's ${day}`);
+        break;
 }
